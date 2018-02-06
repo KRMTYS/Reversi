d79c33ccbb14396b750b68e897011cd5829f5eef
KentaKuramochi <16nm657f@vc.ibaraki.ac.jp>
2018-02-07 03:18:26 +0900

    思考ルーチン：ランダムな位置に置く

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "board.h"
 
 bool play_turn(int,int*);
-void parse_input(int *, int *);
+void player_input(int*, int*);
+void program_input(int*, int*);
 void judge();
 
 int main()
@@ -12,7 +15,10 @@ int main()
     int prev_count = 1;
     int turn = 1;
 
+    srand((unsigned)time(NULL));
+
     init();
+    printf("You are Black\n");
 
     while (play_turn(turn, &prev_count))
     {
@@ -52,7 +58,9 @@ bool play_turn(int turn,int* prev_count)
     while (count > 0)
     {
         printf("[%d] %s: ", turn, (turn % 2 == 1) ? "Black" : "White");
-        parse_input(&x, &y);
+
+        //player_input(&x, &y);
+        program_input(&x, &y);
 
         if (in_board(x, y) || is_none(x, y))
         {
@@ -66,17 +74,24 @@ bool play_turn(int turn,int* prev_count)
     return true;
 }
 
-// parse input
-void parse_input(int *x, int *y)
+void player_input(int* x, int* y)
 {
     char input[4];
     fgets(input, 4, stdin);
 
-    if (!isalpha(input[0]) && !isdigit(input[1]))
+    if (!isalpha(input[0]) || !isdigit(input[1]))
         return;
 
     *x = toupper(input[0]) - '@';
     *y = input[1] - '0';
+}
+
+void program_input(int* x, int* y)
+{
+    *x = rand() % 8 + 1;
+    *y = rand() % 8 + 1;
+
+    printf("%c%c\n", *x + '@', *y + '0');
 }
 
 // count disks and judge
